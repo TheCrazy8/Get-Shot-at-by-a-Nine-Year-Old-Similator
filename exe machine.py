16935d198991ceccb50802af8281d8c1da3cf912
TheCrazy8 <bennettbashford@gmail.com>
2025-08-29 10:43:04 -0400

    s

---
+++ exe machine.py
@@ -0,0 +1,23 @@
+import PyInstaller.__main__
+import os
+from pathlib import Path
+
+# Get the path to the user's home directory
+home_directory = Path.home()
+
+# Construct the path to the Downloads directory
+downloads_directory = home_directory / "Downloads"
+
+# Change the current working directory to the Downloads directory
+os.chdir(downloads_directory)
+
+print(f"Successfully navigated to: {os.getcwd()}")
+
+# Run the script to make Python file into an EXE file
+PyInstaller.__main__.run([
+'bullet hell.py',
+'--add-data=music.mp3:.',
+'--onefile',
+'--windowed',
+'--icon=icon3.ico'
+])
