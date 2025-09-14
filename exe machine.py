f337949cc44da5ea6c249c17d11bd25d28ee0f4f
TheCrazy8 <bennettbashford@gmail.com>
2025-09-13 23:29:38 -0400

    ...

--- exe machine.py
+++ exe machine.py
@@ -1,5 +1,6 @@
 import PyInstaller.__main__
 import os
+import sys
 from pathlib import Path
 
 # Get the path to the user's home directory
@@ -13,11 +14,20 @@ os.chdir(downloads_directory)
 
 print(f"Successfully navigated to: {os.getcwd()}")
 
-# Run the script to make Python file into an EXE file
-PyInstaller.__main__.run([
-'Rift of Memories and Regrets.py',
-'--add-data=music.mp3:.',
-'--onefile',
-'--windowed',
-'--icon=icon3.ico'
-])
+def build():
+	# PyInstaller uses different separator in --add-data (Windows=';', others=':')
+	sep = ';' if os.name == 'nt' else ':'
+	data_args = [
+		f"music.mp3{sep}.",
+		f"lore.txt{sep}."
+	]
+	PyInstaller.__main__.run([
+		'Rift of Memories and Regrets.py',
+		*[f'--add-data={d}' for d in data_args],
+		'--onefile',
+		'--windowed',
+		'--icon=icon3.ico'
+	])
+
+if __name__ == '__main__':
+	build()
